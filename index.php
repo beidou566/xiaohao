9dc441a0efca9007042c702d061176cec3be74bb
beidou566 <beidou566@qq.com>
2017-04-14 18:13:19 +0800

    Revert "1"
    This reverts commit 24a82fb2e9b80472cc605ed2c5a17436505e1ef1.

---
+++ index.php
@@ -0,0 +1,13 @@
+<HTML>
+<HEAD>
+<TITLE>¾­µäÑ­»·Àı×Ó</TITLE>
+</HEAD>
+<BODY>
+<?php
+    for($counter = 1; $counter < 7; $counter++)
+    {
+        echo "<B>counter is ".$counter."</B><BR>\n"; 
+    }
+?>
+</BODY>
+</HTML>
