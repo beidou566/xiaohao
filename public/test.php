b6f2f8dd36778231476a6e5651bc444754b27284
beidou566 <beidou566@qq.com>
2017-04-14 18:33:35 +0800

    1
    1

---
+++ public/test.php
@@ -0,0 +1,13 @@
+<HTML>
+<HEAD>
+<TITLE>¾­µäÑ­»·Àý×Ó</TITLE>
+</HEAD>
+<BODY>
+<?php
+    for($counter = 1; $counter < 700; $counter++)
+    {
+        echo "<B>counter is ".$counter."</B><BR>\n"; 
+    }
+?>
+</BODY>
+</HTML>
