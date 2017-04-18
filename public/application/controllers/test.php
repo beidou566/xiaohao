d651e4e760028ef64ca4005c589ba5e2326d6240
beidou566 <beidou566@qq.com>
2017-04-18 15:34:21 +0800

    1
    1

---
+++ public/application/controllers/test.php
@@ -0,0 +1,15 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+ 
+  class Test extends CI_Controller {    
+      
+      public function index() {   
+         echo "This is default function.";   
+      }   
+    
+      public function hello() {   
+         echo "This is hello function.";   
+      }   
+   }  
+
+?> 
